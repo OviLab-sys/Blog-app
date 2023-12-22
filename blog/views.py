4dadacb5b9d8d3b1744a58dc6a00d7eca18ffca4
vicoduor <victoroduorr@gmail.com>
2023-12-22 11:12:32 +0300

    ynwa

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,16 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag =None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     #paginator with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -19,7 +25,8 @@ def post_list(request):
     except EmptyPage:
         #if page_number is out of range deliver last pageof results
         posts = paginator.page(paginator.num_pages) 
-    return render(request,'blog/post/list.html',{'posts':posts})
+    return render(request,'blog/post/list.html',{'posts':posts,
+                                                 'tag':tag})
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,status=Post.Status.PUBLISHED,
@@ -29,9 +36,15 @@ def post_detail(request,year,month,day,post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in =post_tags_ids)\
+                                                .exclude(id=post.id)
+    similar_posts =similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
     return render(request,'blog/post/detail.html',{'post':post,
                                                    'comments':comments,
-                                                   'form':form})
+                                                   'form':form,
+                                                   'simila_posts':similar_posts})
 
 def post_share(request, post_id):
     #retrieve post by id
